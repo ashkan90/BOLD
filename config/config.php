0cbfb1a906b2f0c4155f25e78ef4dd35e0f178ab
Emirhan <emirhan.1ataman@gmail.com>
2018-08-03 11:28:15 +0300

    declared a lot constants

--- config/config.php
+++ config/config.php
@@ -1,8 +1,22 @@
 <?php
 	define('DEBUG', true);
+
+	define('DB_NAME', 'wrap');
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', '');
+	define('DB_HOST', '127.0.0.1');
+	
+	
+	
+	
+
 	define('DEFAULT_CONTROLLER', 'Home'); // default controller if there isn't one defined in the url 
 	define('DEFAULT_LAYOUT', 'default'); // if no layout is set in the controller use this layout
 
 	define('PROOT', "/bold/"); // set this to '/' for a live server.
 
 	define('SITE_TITLE', 'BOLD Framework'); // this will be used if no site title is set
+
+	define('CURRENT_USER_SESSION_NAME', 'hKgdsfKHsffchlLRQdsdFQBMnfKGSQULO');
+	define('REMEMBER_ME_COOKIE_NAME', 'FJLGJKRLGJF55JKL3J42J3NLSgdlkfGJdjk5ldks');
+	define('REMEMBER_ME_COOKIE_EXPIRY', 604800); // for 30 days expiry
