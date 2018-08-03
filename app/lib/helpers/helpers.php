8aab49131b3bba272a39baf5af5018851ba9cc44
Emirhan <emirhan.1ataman@gmail.com>
2018-08-03 11:26:41 +0300

    added some helper funcs

--- app/lib/helpers/helpers.php
+++ app/lib/helpers/helpers.php
@@ -6,3 +6,15 @@ function dd($data){
 	echo '</pre>';
 	die();
 }
+
+function sanitize($dirty){
+	return htmlentities($dirty, ENT_QUOTES, 'UTF-8');
+}
+
+function _hash($string){
+ 	return password_hash($string, PASSWORD_DEFAULT);
+}
+
+function check_hash($string, $model){
+	return password_verify($string, $model);
+}
